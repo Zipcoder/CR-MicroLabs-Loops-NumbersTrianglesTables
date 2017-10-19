d7d6c99fa620d918ad4ba606382423157f139b6d
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-19 19:59:11 -0400

    Change name of areWeThereYetTest() to areWeThereYet()

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -2,7 +2,8 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class CarRide {
 
-    public String areWeThereYetTest(){
+    public String areWeThereYet()
+    {
         return "";
     }
 
