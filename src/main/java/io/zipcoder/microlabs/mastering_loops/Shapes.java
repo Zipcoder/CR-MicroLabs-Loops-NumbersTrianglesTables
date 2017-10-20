7f3b97ac7776cac78cddf2970fc3042657f4cec5
gpatelos <greg.patselas@gmail.com>
2017-10-20 13:17:36 -0400

    shapes excercises

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -1,17 +1,76 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import com.sun.tools.javac.util.StringUtils;
+
 public class Shapes {
 
     public String triangle(){
-        return "";
+
+        String output = "triangle()\n*** Output ***";
+
+        // OH, by "nested for loops" you meant "substring"
+        // I heard someone say that nested for loops are bad
+
+        /*String breakthisstringintopieces = "\n*****";
+        output += breakthisstringintopieces.substring(1,1);
+
+        for (int i= 2; i<7; i++){
+            output += breakthisstringintopieces.substring(0,i);
+        }
+        */
+
+        for(int row = 1; row<6; row++){
+            output += "\n*";
+
+            for(int col = 1; col<row ; col++){
+
+                output += "*";
+
+            }
+
+        }
+
+
+        return output;
     }
 
-    public String tableSquare(){
-        return "";
+    public String tableSquare(int n){
+
+        String output = "tableSquares(" + n + ")\n*** Output ***\nA " + n + " x " + n + " table square";
+
+        for(int row = 1; row<5; row++){
+            output += "\n|";
+
+            for(int col = 1; col<5 ; col++){
+
+                output +=  String.format("%3d |",col*row);
+
+
+            }
+
+        }
+
+        return output;
     }
 
     public String tableSquares(int n){
-        return "";
+        String output = "tableSquares(" + n + ")\n*** Output ***\nA " + n + " x " + n + " table square";
+
+        for(int row = 1; row<=n; row++){
+            output += "\n|";
+
+            for(int col = 1; col<=n ; col++){
+
+                output +=  String.format("%3d |",col*row);
+
+
+            }
+
+        }
+
+
+
+        return output;
     }
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
@@ -11,18 +11,55 @@ public class ShapesTest {
     @Test
     public void triangleTest(){
         //: Given
+        Shapes shapes = new Shapes();
+        String expected = "triangle()\n*** Output ***\n*\n**\n***\n****\n*****";
 
         //: When
+        String actual = shapes.triangle();
 
         //: Then
+        Assert.assertEquals("that's my \u25B5", expected, actual);
     }
 
     @Test
     public void tableSquareTest(){
         //: Given
+        Shapes shapes = new Shapes();
+        String expected =   "tableSquares(4)\n" +
+                            "*** Output ***\n" +
+                            "A 4 x 4 table square\n" +
+                            "|  1 |  2 |  3 |  4 |\n" +
+                            "|  2 |  4 |  6 |  8 |\n" +
+                            "|  3 |  6 |  9 | 12 |\n" +
+                            "|  4 |  8 | 12 | 16 |";
 
         //: When
+        String actual = shapes.tableSquare(4);
 
         //: Then
+        Assert.assertEquals("that's my square", expected, actual);
+    }
+
+    @Test
+    public void tableSquareNTest(){
+        //: Given
+        Shapes shapes = new Shapes();
+        String expected =   "tableSquares(6)\n" +
+                "*** Output ***\n" +
+                "A 6 x 6 table square\n" +
+                "|  1 |  2 |  3 |  4 |  5 |  6 |\n" +
+                "|  2 |  4 |  6 |  8 | 10 | 12 |\n" +
+                "|  3 |  6 |  9 | 12 | 15 | 18 |\n" +
+                "|  4 |  8 | 12 | 16 | 20 | 24 |\n" +
+                "|  5 | 10 | 15 | 20 | 25 | 30 |\n" +
+                "|  6 | 12 | 18 | 24 | 30 | 36 |";
+
+        //: When
+        String actual = shapes.tableSquares(6);
+
+        //: Then
+        Assert.assertEquals("that's my square", expected, actual);
     }
 }
+
+
