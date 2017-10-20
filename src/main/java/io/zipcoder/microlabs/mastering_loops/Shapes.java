6e289a09b0d9b1fe0e1c0ee625c55a4ffd985d37
logan <loganhanner@gmail.com>
2017-10-20 10:04:28 -0400

    Added code to shapes class

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -3,15 +3,49 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Shapes {
 
-    public String triangle(){
-        return "";
+    public String triangle() {
+
+        String output = "triangle()\n*** Output ***";
+
+        for(int i = 1; i < 6; i++) {
+            output += "\n";
+            for(int j = 0; j < i; j++) {
+                output += "*";
+            }
+        }
+
+        return output;
     }
 
     public String tableSquare(){
-        return "";
+
+        String output = "tableSquare()\n*** Output ***\nA 4x4 table square";
+
+        for(int i = 1; i <= 4; i++) {
+            output += "\n|";
+            int count = i;
+            for(int j = 0; j < 4; j++) {
+                output += " " + count + " |";
+                count += i;
+            }
+        }
+
+        return output;
     }
 
     public String tableSquares(int n){
-        return "";
+
+        String output = "tableSquares()\n*** Output ***\nA 6x6 table square";
+
+        for(int i = 1; i < (n + 1); i++) {
+            output += "\n|";
+            int count = i;
+            for(int j = 0; j < n; j++) {
+                output += " " + count + " |";
+                count += i;
+            }
+        }
+
+        return output;
     }
 }
