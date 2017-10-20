3ea8f9b24d11b2b3540eb40b77af575836dce37b
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-20 07:41:29 -0400

    Create unit test and method code for tableSquare()

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -1,6 +1,5 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-
 public class Shapes {
 
     public String triangle()
@@ -17,8 +16,21 @@ public class Shapes {
         return output;
     }
 
-    public String tableSquare(){
-        return "";
+    public String tableSquare()
+    {/*n the class Shapes, complete the method called tableSquare()
+        so that it uses nested loops to produce a 4x4 table square.*/
+        String output = "tableSquare()\n*** Output ***";
+        for (int i=1; i<=4; i++)
+        {
+            output += "\n";
+            for (int j=1; j<=4; j++)
+            {
+
+                output += "| " + String.format("%2d",(i*j)) + " ";
+            }
+            output += "|";
+        }
+        return output;
     }
 
     public String tableSquares(int n){
--- src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
@@ -22,9 +22,11 @@ public class ShapesTest {
     @Test
     public void tableSquareTest(){
         //: Given
-
+        String expected = "tableSquare()\n*** Output ***\n|  1 |  2 |  3 |  4 |\n|  2 |  4 |  6 |  8 |\n|  3 |  6 |  9 | 12 |\n|  4 |  8 | 12 | 16 |";
+        Shapes shape = new Shapes();
         //: When
-
+        String actual = shape.tableSquare();
         //: Then
+        Assert.assertEquals("These strings are not equal", expected, actual);
     }
 }
