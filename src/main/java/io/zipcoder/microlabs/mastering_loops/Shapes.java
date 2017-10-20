deeb92b4a77f51eab3c35754a85801fe03fa5b9d
zachary <zachary@zipcoders-MacBook-Pro.local>
2017-10-20 09:42:00 -0400

    Fixed underlining/final

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -55,7 +55,7 @@ public class Shapes {
                         output += " " + product + " |";
                     }
                 } else {
-                    output += " " + product + " |";
+                    output += " " + product + " " + "|";
                 }
             }
         }
