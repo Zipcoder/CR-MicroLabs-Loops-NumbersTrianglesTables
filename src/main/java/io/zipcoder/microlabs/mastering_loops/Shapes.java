8ec9e218227129a203d743e69c8f41821381f955
andrew <andrew@zipcoders-MacBook-Pro.local>
2017-10-19 21:40:05 -0400

    Finally done.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -3,15 +3,61 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Shapes {
 
+// This way is better... just saying -______-
+
+
+//    public String triangle(){
+//        String output = "triangle()\n*** Output ***";
+//
+//        String star = "*";
+//
+//        for (int x = 0; x < 5; x++) {
+//            output += "\n" + star;
+//            star+="*";
+//        }
+//
+//        return output;
+//    }
+
     public String triangle(){
-        return "";
+        String output = "triangle()\n*** Output ***";
+
+        String star = "*";
+
+        for (int x = 0; x < 5; x++) {
+            output += "\n" + star;
+            for (int y = 0; y < 1; y ++) {
+                //output += "\n" + star;
+                star+="*";
+            }
+
+
+        }
+
+        return output;
     }
 
     public String tableSquare(){
-        return "";
+        String output = "A 4 x 4 table square";
+        for (int row = 1; row <= 4; row++) {
+            output += "\n|";
+            for (int column = 1; column <= 4; column++) {
+                output += String.format("%3d |", row*column);
+            }
+        }
+
+        return output;
     }
 
     public String tableSquares(int n){
-        return "";
+        String output = "A 6 x 6 table square";
+        for (int row = 1; row <= n; row++) {
+            output += "\n|";
+            for (int column = 1; column <= n; column++) {
+                output += String.format("%3d |", row*column);
+            }
+        }
+
+        return output;
     }
 }
