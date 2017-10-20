57336b5af668583ef7d83f6f185cb4cb9d0300e0
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-20 11:26:20 -0400

    Create unit test and code for CarRide class

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,10 +1,29 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-public class CarRide {
+import java.util.Scanner;
 
-    public String areWeThereYet()
-    {
-        return "";
+public class CarRide {/*In the class CarRide, complete the class so that it returns
+    that outputs "Are we there yet?" and then waits for input.
+    If the input is "Yes" the program outputs "Good!" and exits,
+    otherwise the program loops. The Unit Tests is not provided
+    for you, you must complete it.*/
+
+    public static void main(String[] args) {
+        areWeThereYet();
     }
 
+    public static void areWeThereYet() {//Prompt for input, then test it
+        Scanner in = new Scanner(System.in);
+        do
+            System.out.print("Are we there yet? ");
+        while (!"Good!".equals(testInput(in.nextLine())));
+        System.out.println("Good!");
+    }
+
+    public static String testInput(String input)
+    {//Compare input to YES and return true or false
+        if ("Yes".equalsIgnoreCase(input))
+            return "Good!";
+        return "";
+    }
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/CarRideTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/CarRideTest.java
@@ -1,9 +1,37 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CarRideTest {
 
+    @Test
+    public void testInputTest() {
+        // Given
+        String userInput = "Yes";
+        String expected = "Good!";
+        CarRide carRide = new CarRide();
+
+        // When
+        String actual = carRide.testInput(userInput);
+
+        // Then
+        Assert.assertEquals("The strings are not the same",expected, actual);
+    }
+    /*
+    @Test
+    public void testInputTest()
+    {/* Tested method returns true if passed "Yes", otherwise false. /
+        //: Given
+        boolean inputYes = true;
+        String stringYes = "Yes";
+
+        //: When
+        boolean returnedYes = CarRide.testInput(stringYes);
+
+        //: Then
+        Assert.assertTrue("Other was input", returnedYes);
+    }
+*/
 
 }
