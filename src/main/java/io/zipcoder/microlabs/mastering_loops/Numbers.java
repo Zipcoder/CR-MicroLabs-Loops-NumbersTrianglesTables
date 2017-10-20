db0b403ca21e6349d84cc7ef66deb764851fb687
raul <raul@zipcoders-MacBook-Pro.local>
2017-10-19 20:49:16 -0400

    7 task finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -1,30 +1,94 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import org.junit.Assert;
+
 public class Numbers {
 
     public String oneToTen(){
-        return "";
+
+        String numbers = "*** Output ***\noneToTen()";
+
+        for(int i = 1;i<=10;i++){
+
+        numbers += "\n" +i;
+
+        }
+
+        return numbers;
     }
 
     public String oddNumbers(){
-        return "";
+
+        String numbers = "*** Output ***\noddNumbers()";
+
+        for(int i = 1;i<=20;i++){
+
+            if(i%2 != 0){
+                numbers +=  "\n" +i;
+            }
+        }
+
+        return numbers;
     }
 
     public String squares(){
-        return "";
+
+        String numbers = "*** Output ***\nsquaresNumbers()";
+
+        for(int i = 1;i<=10;i++){
+
+                numbers +=  "\n" + i * i;
+
+
+        }
+
+        return numbers;
+
     }
 
     public String random4(){
-        return "";
+
+        String numbers = "*** Output ***\nRandom4()";
+
+        for(int i = 0;i<4;i++){
+
+                numbers +=  "\n" + (int)(Math.random()*10);
+
+        }
+
+        return numbers;
     }
 
     public String even(int n){
-        return "";
+
+        String numbers = "*** Output ***\neven()";
+
+        for(int i = 1;i<=8;i++){
+
+            if(i%2 == 0){
+                numbers +=  "\n" +i;
+            }
+        }
+
+        return numbers;
+
     }
 
     public String powers(int n){
-        return "";
+
+        String numbers = "*** Output ***\nsquaresNumbers()";
+
+
+        for(int i = 1;i<=8;i++){
+
+                numbers += "\n" + (int)Math.pow(2,i);
+        }
+        return numbers;
     }
 
+    public void powersTest(){
+
+
+    }
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
@@ -21,46 +21,77 @@ public class NumbersTest {
     @Test
     public void oddNumbersTest(){
         //: Given
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\noddNumbers()\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
 
         //: When
+        String actual = numbers.oddNumbers();
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
+
     }
 
     @Test
     public void squaresTest(){
         //: Given
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\nsquaresNumbers()\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
 
         //: When
+        String actual = numbers.squares();
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
+
     }
 
     @Test
     public void random4Test(){
         //: Given
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\nRandom4() 1 < x < 10 ";
 
         //: When
+        String actual = numbers.random4();
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
+
     }
 
     @Test
     public void evenTest(){
         //: Given
 
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\neven()\n2\n4\n6\n8";
         //: When
-
+        String actual = numbers.even( 8);
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
+
     }
 
     @Test
     public void powersTest(){
         //: Given
-
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\nsquaresNumbers()\n2\n4\n8\n16\n32\n64\n128\n256";
         //: When
+        String actual = numbers.powers(8);
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
+    }
+    @Test
+    public void CarRide(String answer){
+
+        Numbers number = new Numbers();
+        String expected = "Yes";
+        Assert.assertEquals("The two strings are equal", expected, actual);
+
+
     }
 
 }
