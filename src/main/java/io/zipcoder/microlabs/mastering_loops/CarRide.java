4566251941838c638d3ebf545dc8651a2b59483c
samuel <samuel@zipcoders-MacBook-Pro.local>
2017-10-20 14:38:46 -0400

    Lab Are we there yet?

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,15 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+
 public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+        public String areWeThereYetTest(String usrAnswer){
+
+            String output = "*** Output ***\nCarRide\nAre We There Yet?";
+            if (usrAnswer.equals("Yes")) {
+            output = "Good";
+        }
+        return output;
     }
 
 }
