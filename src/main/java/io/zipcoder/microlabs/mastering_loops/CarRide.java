aa19e68ea9e595240e4d6295289b532e8b8cd41f
wesley <wesley@zipcoders-MBP-8.fios-router.home>
2017-10-19 23:24:00 -0400

    All done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,16 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Scanner;
+
 public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+    public String areWeThereYet(String input){
+        String output = "Are we there yet?";
+        if("yes".equalsIgnoreCase(input)){
+            output = "Good!";
+        }
+
+        return output;
     }
 
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -4,27 +4,62 @@ package io.zipcoder.microlabs.mastering_loops;
 public class Numbers {
 
     public String oneToTen(){
-        return "";
+        String output = "*** Output ***\noneToTen()";
+        for(int i = 1;i<= 10 ;i++){
+            output += "\n" + i;
+        }
+        return output;
     }
 
     public String oddNumbers(){
-        return "";
+        String output = "oddNumbers()\n*** Output ***";
+        for(int i = 1; i<20;i++){
+            if((i%2)==1){
+                output += "\n" + i;
+            }
+        }
+        return output;
     }
 
     public String squares(){
-        return "";
+        String output = "squares()\n*** Output ***";
+        for(int i = 1;i<=10;i++){
+            int squareNum = i * i;
+            output+= "\n" + squareNum;
+        }
+        return output;
     }
 
     public String random4(){
-        return "";
+        int randNum;
+        String output = "random4()\n*** Output ***";
+        for(int i = 0;i<4;i++){
+            randNum = (int)(Math.random()*10)+1;
+            //randNum = 10;
+            output+= randNum;
+            System.out.println(randNum);
+        }
+
+        return output;
+
     }
 
     public String even(int n){
-        return "";
+        String output = "even(20)\n*** Output ***";
+        for(int i = 1;i<n;i++){
+            if((i%2)==0) {
+                output += "\n" + i;
+            }
+        }
+        return output;
     }
 
     public String powers(int n){
-        return "";
+        String output = "power(8)\n*** Output ***";
+        for(int i = 1;i<=n;i++){
+            output += "\n" + (int)Math.pow(2,i);
+        }
+        return output;
     }
 
 }
