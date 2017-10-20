0174870b64e2973273d934d459072acd30a69156
andrew <andrew@zipcoders-MacBook-Pro.local>
2017-10-20 12:02:52 -0400

    Correct and tested AreWeThereYet

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 
 public class CarRide {
 
-    public boolean areWeThere(String input) {
+    public static void main(String[] args) {
+        CarRide car = new CarRide();
+        Scanner sc = new Scanner(System.in);
+        String answer = null;
+
+        do {
+            System.out.println("Are we there yet?");
+            answer = sc.next();
+
+        } while (!car.areWeThere(answer));
+
+        System.out.println(returnAnswer(car.areWeThere(answer)));
+    }
+
+    public static boolean areWeThere(String input) {
         return "Yes".equals(input);
     }
 
-    public String returnAnswer(boolean input) {
+    public static String returnAnswer(boolean input) {
         return input ? "Good" : "Are we there yet?";
     }
 }
