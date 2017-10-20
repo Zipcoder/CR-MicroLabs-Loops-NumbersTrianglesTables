584b938b3415dbbbb3be88b1ae3699491a6fff71
logan <loganhanner@gmail.com>
2017-10-19 20:03:44 -0400

    Added CarRide class

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,35 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-public class CarRide {
+import java.util.Scanner;
 
-    public String areWeThereYetTest(){
-        return "";
+public class CarRide {
+    /*
+    public String areWeThereYetFull() {
+        String prompt = "Are we there yet?";
+        String answer;
+        String output = "";
+        boolean there = false;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        do{
+            System.out.print(prompt);
+            answer = keyboard.nextLine();
+            output += prompt + "\n" + answer;
+            if("Yes".equalsIgnoreCase(answer)) {
+                there = true;
+            }
+        }while(there == false);
+        return output;
+    }
+    */
+
+    public boolean areWeThereYet(String str) {
+        boolean valid = false;
+        if ("Yes".equalsIgnoreCase(str)) {
+            valid = true;
+        }
+        return valid;
     }
 
 }
