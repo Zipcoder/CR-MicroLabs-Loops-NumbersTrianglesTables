26f2a38029ce1d3566896fecda2fb0305cebcea1
pavel <pavel@zipcoders-MacBook-Pro.local>
2017-10-19 20:39:58 -0400

    All done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -4,14 +4,38 @@ package io.zipcoder.microlabs.mastering_loops;
 public class Shapes {
 
     public String triangle(){
-        return "";
+        String output = "triangle()\n***Output***";
+
+        for(int i = 0; i < 5; i++){
+            output += "\n";
+            for(int j = 0; j < (i+1); j++){
+                output += "*";
+            }
+        }
+        return output;
     }
 
     public String tableSquare(){
-        return "";
+        String output = "tableSquare()\n***Output***";
+
+        for(int i = 1; i <= 4; i++){
+            output += "\n|";
+            for(int j = 1; j <= 4; j++){
+                output += String.format("%3d |", (i*j));
+            }
+        }
+        return output;
     }
 
     public String tableSquares(int n){
-        return "";
+        String output = "tableSquares(6)\n***Output***";
+
+        for(int i = 1; i <= n; i++){
+            output += "\n|";
+            for(int j = 1; j <= n; j++){
+                output += String.format("%3d |", (i*j));
+            }
+        }
+        return output;
     }
 }
