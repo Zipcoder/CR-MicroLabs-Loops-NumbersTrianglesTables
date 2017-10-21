80f20dc930a15d5335a47516c61d2d5945ebd331
rikeroleg <rikeroleg@gmail.com>
2017-10-21 14:43:46 -0400

    Loops1

--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
@@ -21,19 +21,23 @@ public class NumbersTest {
     @Test
     public void oddNumbersTest(){
         //: Given
-
+Numbers oddNumbers=new Numbers ();
+String expected = "*** Output ***\noddNumbers()\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
         //: When
-
+String actual = oddNumbers.oddNumbers();
         //: Then
+Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
     public void squaresTest(){
         //: Given
-
+Numbers squares=new Numbers();
+String expected = "squares()\n*** Output ***\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
         //: When
-
+String actual = squares.squares();
         //: Then
+Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
@@ -48,10 +52,12 @@ public class NumbersTest {
     @Test
     public void evenTest(){
         //: Given
-
+Numbers evenTest=new Numbers();
+String expected = "even(20)\n*** Output ***\n2\n4\n6\n8\n10\n12\n14\n16\n18";
         //: When
-
+String actual = evenTest.even(20);
         //: Then
+Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
