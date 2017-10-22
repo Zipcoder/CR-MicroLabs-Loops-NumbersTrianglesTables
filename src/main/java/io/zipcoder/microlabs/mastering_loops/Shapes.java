4db568c4cb6f77f8b59a5d313d6eabc860e07b04
donald f <donaldf@zipcoders-MacBook-Pro-4.local>
2017-10-22 09:48:21 -0400

    almost done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -22,10 +22,38 @@ public class Shapes {
     }
 
     public String tableSquare(){
-        return "";
+
+        int tableSizeOne = 4;
+        String output = "*** Output ***\nA 4x4 table square\n";
+
+        for(int i=1; i<=tableSizeOne; i++){
+            int num = i;
+            output += "| " + num + " |";
+            for(int j=1; j<=tableSizeOne -1; j++){
+               num += i;
+               output += " " + num + " |";
+            }
+            output += "\n";
+        }
+
+        return output;
     }
 
     public String tableSquares(int n){
-        return "";
+
+        String output = "*** Output ***\nA 6x6 table square()\n";
+
+        for(int i=1; i<=n; i++){
+            int num = i;
+            output += "| " + num + " |";
+            for(int j=1; j<=n -1; j++){
+                num += i;
+                output += " " + num + " |";
+            }
+            output += "\n";
+        }
+
+
+        return output;
     }
 }
