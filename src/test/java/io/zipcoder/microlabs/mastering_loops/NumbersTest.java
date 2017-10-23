ee6693d61c86b4d932c5b692280f029d354c7751
linda <linda@zipcoders-MacBook-Pro-5.local>
2017-10-23 13:34:07 -0400

    loops,shapes,carride

--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
@@ -8,32 +8,40 @@ public class NumbersTest {
     @Test
     public void oneToTenTest(){
         //: Given
-        Numbers numbers = new Numbers();
-        String expected = "*** Output ***\noneToTen()\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+            Numbers numbers = new Numbers();
+            String expected = "*** Output ***\noneToTen()\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
 
         //: When
-        String actual = numbers.oneToTen();
+            String actual = numbers.oneToTen();
 
         //: Then
-        Assert.assertEquals("The two strings are equal", expected, actual);
+            Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
     public void oddNumbersTest(){
         //: Given
+            Numbers numbers = new Numbers();
+            String expected = "*** Output ***\noddNumbers()\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
 
         //: When
+            String actual = numbers.oddNumbers();
 
         //: Then
+            Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
     public void squaresTest(){
         //: Given
+            Numbers numbers =  new Numbers();
+            String expected = "*** Output ***\nsquares()\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
 
         //: When
+            String actual = numbers.squares();
 
         //: Then
+            Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
@@ -48,19 +56,28 @@ public class NumbersTest {
     @Test
     public void evenTest(){
         //: Given
+            int n = 20;
+                Numbers numbers = new Numbers();
+                String expected = "*** Output ***\neven(n)\n2\n4\n6\n8\n10\n12\n14\n16\n18";
 
         //: When
+                String actual = numbers.even(n);
 
         //: Then
+                Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
     public void powersTest(){
         //: Given
-
+            int n = 8;
+                Numbers numbers = new Numbers();
+                String expected = "*** Output ***\npowers(n)\n2\n4\n8\n16\n32\n64\n128\n256";
         //: When
+            String actual = numbers.powers(n);
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
 }
