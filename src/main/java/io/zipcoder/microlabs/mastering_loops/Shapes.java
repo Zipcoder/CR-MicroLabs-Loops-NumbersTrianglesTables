796d0a4a81fe4cffc695fc930fed90f92e705504
samuel <samuel@zipcoders-MacBook-Pro.local>
2017-10-23 12:40:52 -0400

    Project Table Squares 1 Project Table Squares

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -3,15 +3,72 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Shapes {
 
-    public String triangle(){
-        return "";
+
+
+    public String triangle() {
+
+        String output = "*** Output ***\nTriangle";
+
+        int lines =6;
+        for (int i =0; i<5; i++){
+            String star = "";
+        for (int k=0; k <= i; k++) {
+                star += "*";
+            }
+            output += "\n" + star;
+        }
+            return output;
+        }
+
+    public String tableSquare() {
+        String output = "*** Output ***\ntableSquare()";
+
+        for (int l = 1; l < 5; l++) {
+            int newNum = 0;
+            output += "\n";
+            for (int cnt = 0; cnt < 4; cnt++) {
+                newNum = newNum + l;
+                output += "|" + newNum;
+
+            }      output += "|";
+
+                }
+                return output;
+            }
+
+    public String tableSquares(int n) {
+        String input = "*** Output ***\ntableSquare()";
+
+        for(int i = 1; i <=6; i++){
+            input += "\n|";
+
+            for ( int j = 1; j<=6; j++){
+                if (j * i < 10 && j == 1)
+                input = String.format("%s%d|", input, j*i);
+                else if (j * i < 10)
+                    input = String.format("%s%d|", input, j*i);
+                else
+                    input = String.format("%s%d|", input, j*i);
+
+            }
+        }
+
+        return input;
     }
 
-    public String tableSquare(){
-        return "";
-    }
 
-    public String tableSquares(int n){
-        return "";
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
