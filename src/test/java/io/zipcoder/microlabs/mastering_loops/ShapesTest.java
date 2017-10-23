d0a0ad8b5328bd9c993186b508fab877182011e0
samuel <samuel@zipcoders-MacBook-Pro.local>
2017-10-23 12:43:08 -0400

    r Project Table Squares

--- src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
@@ -7,22 +7,46 @@ import org.junit.Assert;
 public class ShapesTest {
 
 
-
     @Test
-    public void triangleTest(){
+    public void trianglTest()
+    {
         //: Given
+        Shapes triangle = new Shapes();
+        String expected = "*** Output ***\nTriangle\n*\n**\n***\n****\n*****";
 
         //: When
+        String actual = triangle.triangle();
 
         //: Then
+        Assert.assertEquals("Yes", expected, actual);
     }
 
     @Test
-    public void tableSquareTest(){
+    public void tableSquareTest() {
         //: Given
+        Shapes square = new Shapes();
+        String expected = "*** Output ***\ntableSquare()\n|1|2|3|4|\n|2|4|6|8|\n|3|6|9|12|\n|4|8|12|16|";
 
         //: When
+        String actual = square.tableSquare();
 
         //: Then
+        Assert.assertEquals("Yes", expected, actual);
     }
-}
+
+        @Test
+        public void tableSquaresTest() {
+            //: Given
+            Shapes squares = new Shapes();
+            String expected = "*** Output ***\ntableSquare()\n|1|2|3|4|5|6|\n|2|4|6|8|10|12|\n|3|6|9|12|15|18|\n|4|8|12|16|20|24|\n|5|10|15|20|25|30|\n|6|12|18|24|30|36|";
+
+            //: When
+            String actual = squares.tableSquares(6);
+
+            //: Then
+            Assert.assertEquals("Yes", expected, actual);
+
+
+        }
+    }
+
