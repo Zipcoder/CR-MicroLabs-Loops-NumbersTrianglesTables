abc5b58ab39b99f8246366fe2746a222f4054823
lina <lina@zipcoders-MacBook-Pro.local>
2017-10-23 17:01:44 -0400

    loops

---
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Main.java
@@ -0,0 +1,10 @@
+package io.zipcoder.microlabs.mastering_loops;
+
+public class Main {
+    public static void main(String[] args){
+        CarRide car = new CarRide();
+        String test = car.areWeThereYetTest("");
+        car.areWeThereYetTestt();
+        System.out.println(test);
+    }
+}
--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -4,15 +4,29 @@ package io.zipcoder.microlabs.mastering_loops;
 public class Numbers {
 
     public String oneToTen(){
-        return "";
+        String output = "oneToTen()\n*** Output ***";
+        for(int i=1;i<=10;i++ ){
+            output += "\n" + i;
+        }
+        return output;
     }
 
     public String oddNumbers(){
-        return "";
+        String output = "oddNumbers()\n*** Output ***";
+        for(int i=1; i<20; i++){
+            if(i%2 != 0){
+                output += "\n" +i;
+            }
+        }
+        return output;
     }
 
     public String squares(){
-        return "";
+        String output ="squares()\n*** Output ***";
+        for(int i=1; i<=10;i++){
+            output += "\n" + i *i;
+        }
+        return output;
     }
 
     public String random4(){
@@ -20,11 +34,22 @@ public class Numbers {
     }
 
     public String even(int n){
-        return "";
+        //return even number from 2-18
+        n=20;
+        String output = "even(20)\n*** Output ***";
+        for(int i=1; i < n;i++){
+            if(i % 2 == 0) output += "\n" + i;
+        }
+        return output;
     }
 
     public String powers(int n){
-        return "";
+        //return a string that's power of 2 from 2^1...2^n, n < 8
+        String output = "powers(8)\n*** Output ***";
+        for(n=1; n<=8;n++ ){
+            output += "\n" + (int)Math.pow(2,n);
+        }
+        return output;
     }
 
 }
