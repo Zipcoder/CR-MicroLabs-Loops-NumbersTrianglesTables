92aee78fc9c49be6801c3e623585706ac9a45fb1
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-02-07 16:39:45 -0500

    First Commit

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,18 +2,74 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        for (int i = 1; i<= 10; i++){
-            System.out.printf("%5s" ,i +" |");
+        StringBuilder answer = new StringBuilder();
+
+        int columnCounter = 1;
+        int rowCounter = 1;
+        int input = 1;
+        for (int i = 1; i<= 25; i++){
+
+            String cell = String.format("%3d |", input);
+            answer.append(cell);
+            columnCounter++;
+            input = rowCounter*columnCounter;
+            if (columnCounter == 6){
+                answer.append("\n");
+                columnCounter = 1;
+                rowCounter++;
+                input = rowCounter;
+
+            }
         }
 
-        return null;
+        return answer.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder answer = new StringBuilder();
+
+        int columnCounter = 1;
+        int rowCounter = 1;
+        int input = 1;
+        for (int i = 1; i<= 100; i++){
+
+            String cell = String.format("%3d |", input);
+            answer.append(cell);
+            columnCounter++;
+            input = rowCounter*columnCounter;
+            if (columnCounter == 11){
+                answer.append("\n");
+                columnCounter = 1;
+                rowCounter++;
+                input = rowCounter;
+
+            }
+        }
+
+        return answer.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder answer = new StringBuilder();
+
+        int columnCounter = 1;
+        int rowCounter = 1;
+        int input = 1;
+        for (int i = 1; i<= tableSize*tableSize; i++){
+
+            String cell = String.format("%3d |", input);
+            answer.append(cell);
+            columnCounter++;
+            input = rowCounter*columnCounter;
+            if (columnCounter == tableSize+1){
+                answer.append("\n");
+                columnCounter = 1;
+                rowCounter++;
+                input = rowCounter;
+
+            }
+        }
+
+        return answer.toString();
     }
 }
