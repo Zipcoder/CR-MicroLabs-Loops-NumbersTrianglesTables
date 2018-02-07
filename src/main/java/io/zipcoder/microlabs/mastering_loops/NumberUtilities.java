8946cb71dba20d92e6c126f1c4fb6b1f3bc4874c
vvmk <vincentmasiello@gmail.com>
2018-02-07 16:14:45 -0500

    fix even/odd tests

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,7 +3,12 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-       return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i=start;i < stop;i++) {
+            if (i % 2 == 0)
+                sb.append(i);
+        }
+        return sb.toString();
     }
 
 
