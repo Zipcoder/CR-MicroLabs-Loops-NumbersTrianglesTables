0757df75c094c69d625db80f7c333205b6090a56
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-07 15:34:33 -0500

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
@@ -2,10 +2,13 @@ package io.zipcoder.microlabs.mastering_loops;
 
 import java.util.Scanner;
 
+import static io.zipcoder.microlabs.mastering_loops.NumberUtilities.getSquareNumbers;
+
 /**
  * Created by leon on 1/31/18.
  */
 public class MainApp {
     public static void main(String[] args) {
+        System.out.println(getSquareNumbers(5, 20, 5));
     }
 }
