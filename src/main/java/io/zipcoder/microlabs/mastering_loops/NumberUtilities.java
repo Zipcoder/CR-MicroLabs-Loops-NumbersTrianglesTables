536dd35a0c8f9639e683553db6011220afa6ec4e
Garrett Arant <garrettarant@MacBook-Pro-4.local>
2018-02-07 15:33:50 -0500

    Third of the way done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,26 +3,84 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder evenNumbers = new StringBuilder();
+
+        for(int i = start; i < stop; i++){
+
+            if (i % 2 == 0){
+                evenNumbers.append(i);
+            }
+        }
+
+
+        return evenNumbers.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder oddNumbers = new StringBuilder();
+
+        for(int i = start; i < stop; i++){
+
+            if (i % 2 != 0){
+                oddNumbers.append(i);
+            }
+        }
+
+
+        return oddNumbers.toString();
+
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        StringBuilder squareNumbers = new StringBuilder();
+
+        for(int i = start; i < stop; i += step){
+
+
+
+                squareNumbers.append((int)(Math.pow(i,2)));
+            }
+
+
+        return squareNumbers.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        StringBuilder rangeNumbers = new StringBuilder();
+
+        for(int i = start; i < stop; i += step){
+
+
+
+            rangeNumbers.append(i);
+        }
+
+
+        return rangeNumbers.toString();
+
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder exponentNumbers = new StringBuilder();
+
+        for(int i = start; i < stop; i += step){
+
+
+
+            exponentNumbers.append((int)(Math.pow(i,exponent)));
+        }
+
+
+        return exponentNumbers.toString();
     }
+
 }
+
