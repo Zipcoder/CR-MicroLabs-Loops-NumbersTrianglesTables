125ab67b6315cff9ac84d4231a34f0214a97ed91
ghost1497 <rfrankie04@gmail.com>
2018-02-07 17:13:59 -0500

    I was helping someone so i made gthe view easier

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -2,19 +2,19 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TriangleUtilities {
 
+    public static String getRow(int numberOfStars) {
+        String output = "";
+        for (int i = 0; i < numberOfStars; i++){
+            output += "*";
+        }
+        return output;
+    }
+
     public static String getTriangle(int numberOfRows) {
         String output = "";
         for (int i = 0; i < numberOfRows; i++){
             output += getRow(i+1);
             output += "\n";
-        }
-        return output;
-    }
-
-    public static String getRow(int numberOfStars) {
-        String output = "";
-        for (int i = 0; i < numberOfStars; i++){
-            output += "*";
         }
         return output;
     }
