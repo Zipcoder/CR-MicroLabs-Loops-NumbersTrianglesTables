11f91527ad875e064be77106b53bb6878b4b2b99
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-02-07 19:33:49 -0500

    Num Utility test

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,26 +3,55 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder even = new StringBuilder();
+
+        for (int i = start; i < stop; i++){
+           if((i % 2) == 0){
+                even.append(i);
+           }
+       }
+        return even.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder odd = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if ((i % 2) != 0) {
+                odd.append(i);
+            }
+        }
+        return odd.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        StringBuilder square = new StringBuilder();
+        for(int i = start; i < stop; i+= step){
+            int squaredNum = (int)Math.pow(i, 2);
+                square.append(squaredNum);
+            }
+        return square.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder range = new StringBuilder();
+        for(int i = start; i < stop; i+= step){
+            range.append(i);
+        }
+        return range.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder expo = new StringBuilder();
+        for(int i = start; i < stop; i += step){
+            int expoVar = exponent;
+            int squaredNum = (int)Math.pow(i, expoVar);
+            expo.append(squaredNum);
+        }
+        return expo.toString();
     }
 }
