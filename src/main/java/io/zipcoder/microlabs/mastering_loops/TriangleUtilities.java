2d693d6c88d61d38b918c9014c6e4b185266b57a
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-02-07 21:04:02 -0500

    Multi table

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,10 +3,12 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
+
         return null;
     }
 
     public static String getRow(int numberOfStars) {
+
         return null;
     }
 
