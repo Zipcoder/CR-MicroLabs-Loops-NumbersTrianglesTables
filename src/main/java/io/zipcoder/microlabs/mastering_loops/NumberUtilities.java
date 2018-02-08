40e30491c590936c74820edd9d250c3a1eec28ed
Peter McCormick <petermccormick@zipcoders-MacBook-Pro-109.local>
2018-02-08 07:54:46 -0500

    numbers util

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -2,23 +2,57 @@ package io.zipcoder.microlabs.mastering_loops;
 
 
 public class NumberUtilities {
+
+
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder evenNumbers = new StringBuilder();
+        for (int i = start; i < stop ; i++) {
+            if(i % 2 == 0)
+            {
+                evenNumbers.append(i);
+            }
+
+        }
+
+
+        return evenNumbers.toString();
+
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder builderOddNumbers = new StringBuilder();
+        for (int i = start; i < stop ; i++) {
+            if(!(i % 2 == 0))
+            {
+                builderOddNumbers.append(i);
+            }
+
+        }
+
+
+        return builderOddNumbers.toString();
+
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        StringBuilder squareNumbersBuilder = new StringBuilder();
+        for (int i = start; i <stop ; i = i + step) {
+            int square = i * i;
+            squareNumbersBuilder.append(square);
+        }
+        return squareNumbersBuilder.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder getRangeBuilder = new StringBuilder();
+        for (int i = start; i <stop ; i = i + step) {
+
+            getRangeBuilder.append(i);
+        }
+        return getRangeBuilder.toString();
     }
 
 
