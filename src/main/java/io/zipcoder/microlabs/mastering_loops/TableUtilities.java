73a5c6e4efc6e780d5b345c28f83ff081a6b9892
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-02-08 09:08:19 -0500

    Tryin those angles

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,45 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        StringBuilder tree = new StringBuilder();
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                tree.append(String.format("%3d |", (i*j)));
+
+            }
+            tree.append("\n");
+        }
+        return tree.toString();
+
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        StringBuilder tree = new StringBuilder();
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                tree.append(String.format("%3d |", (i*j)));
+
+            }
+            tree.append("\n");
+        }
+        return tree.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder tree = new StringBuilder();
+
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                tree.append(String.format("%3d |", (i*j)));
+
+            }
+            tree.append("\n");
+        }
+        return tree.toString();
     }
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumberUtilitiesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumberUtilitiesTest.java
@@ -35,23 +35,10 @@ public class NumberUtilitiesTest {
         Assert.assertEquals(expected, actual);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Test
     public void testGetEvenNumbers() {
         // : Given
-        String expected = "5791113151719";
+        String expected = "681012141618";
         int start = 5;
         int stop = 20;
 
@@ -65,7 +52,7 @@ public class NumberUtilitiesTest {
     @Test
     public void testGetOddNumbers() {
         // : Given
-        String expected = "681012141618";
+        String expected = "5791113151719";
         int start = 5;
         int stop = 20;
         int step = 5;
