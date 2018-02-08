77c3af3183abe6ff176bf0116e5a9e3a23200a1e
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-08 13:47:31 -0500

    Tri Guys

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,60 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        System.out.println("Get Triangle");
+        String resultString = "";
+        System.out.println(resultString);
+        for (int i = 1; i < numberOfRows; i++){
+
+            String rowofStars = "";
+            for (int j = 0; j < i; j++){
+                rowofStars = rowofStars + "*";
+
+            }
+            resultString = resultString + rowofStars + "\n";
+        }
+
+        return resultString;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        System.out.println("Get Row");
+        String resultString = "";
+       for (int i = 0; i < numberOfStars; i++){
+           resultString = resultString + "*";
+       }
+       return resultString;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        System.out.println("Get Small Triangle");
+        String resultString = "";
+        System.out.println(resultString);
+        for (int i = 1; i < 5; i++){
+            String rowofStars = "";
+            for (int j = 0; j < i; j++){
+                rowofStars = rowofStars + "*";
+
+            }
+            resultString = resultString + rowofStars + "\n";
+        }
+
+        return resultString;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        System.out.println("Get Large Triangle");
+        String resultString = "";
+        System.out.println(resultString);
+        for (int i = 1; i < 10; i++){
+            String rowofStars = "";
+            for (int j = 0; j < i; j++){
+                rowofStars = rowofStars + "*";
+
+            }
+            resultString = resultString + rowofStars + "\n";
+        }
+
+        return resultString;
     }
 }
