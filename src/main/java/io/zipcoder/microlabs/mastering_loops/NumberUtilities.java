58627cd7b0af35adf9a6726b6b2109372000b286
vvmk <vincentmasiello@gmail.com>
2018-02-07 22:37:00 -0500

    write table utils, all tests pass

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -14,13 +14,7 @@ public class NumberUtilities {
 */
 
     public static String getOddNumbers(int start, int stop) {
-        StringBuilder sb = new StringBuilder();
-        for (int i=start;i < stop;i++) {
-            if (i % 2 != 0) {
-                sb.append(i);
-            }
-        }
-        return sb.toString();
+        return getRange((start % 2 != 0) ? start : start+1, stop, 2);
     }
 
     public static String getSquareNumbers(int start, int stop, int step) {
