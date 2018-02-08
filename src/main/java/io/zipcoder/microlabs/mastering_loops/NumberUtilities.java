804d4c595c46adc2ed2c423e3941d38e30da1a15
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-07 19:19:25 -0500

    solved numbers

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,26 +3,58 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        String evens = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                evens += Integer.toString(i);
+            }
+        }
+
+        return evens;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        String odds = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 != 0) {
+                odds += Integer.toString(i);
+            }
+        }
+
+        return odds;
+
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        String roots = "";
+        for (int i = start; i < stop; i += step) {
+            roots += Integer.toString(i * i);
+        }
+        return roots;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        String rangeString = "";
+        for (int i = start; i < stop; i += step) {
+            rangeString += Integer.toString(i);
+        }
+        return rangeString;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+
+        String exponentString = "";
+        for (int i = start; i < stop; i += step) {
+            exponentString += (int) Math.pow(i, exponent);
+        }
+        return exponentString;
     }
 }
