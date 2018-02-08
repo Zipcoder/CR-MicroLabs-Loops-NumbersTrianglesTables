250f459bcaa69218b374697709e00244079d521b
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-3.local>
2018-02-07 23:51:16 -0500

    Deleted code block, utilized functions

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,29 +1,15 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
+
     public static String getSmallMultiplicationTable() {
 
-        StringBuilder matrix = new StringBuilder();
-
-        for(int row = 1; row <= 5; row++) {
-            for(int col = 1; col <= 5; col++){
-                matrix.append(String.format("%3d |", row * col));
-            }
-            matrix.append("\n");
-        }
-        return matrix.toString();
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        StringBuilder matrix = new StringBuilder();
 
-        for(int row = 1; row <= 10; row++) {
-            for(int col = 1; col <= 10; col++){
-                matrix.append(String.format("%3d |", row * col));
-            }
-            matrix.append("\n");
-        }
-        return matrix.toString();
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -5,6 +5,7 @@ public class TriangleUtilities {
     public static String getTriangle(int numberOfRows) {
 
         String triangleContainer = "";
+
         for (int row=1; row < numberOfRows; row++)
         {
             for (int column=0; column<row; column++)
@@ -19,7 +20,8 @@ public class TriangleUtilities {
     public static String getRow(int numberOfStars) {
 
         String triangleContainer = "";
-        for (int row=0; row < 20; row++)
+
+        for (int row=0; row < numberOfStars; row++)
         {
             triangleContainer += "*";
         }
@@ -28,29 +30,11 @@ public class TriangleUtilities {
 
     public static String getSmallTriangle() {
 
-        String triangleContainer = "";
-        for (int row=1; row < 5; row++)
-        {
-            for (int column=0; column<row; column++)
-            {
-                triangleContainer += "*";
-            }
-            triangleContainer += "\n";
-        }
-        return triangleContainer;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
 
-        String triangleContainer = "";
-        for (int row=1; row < 10; row++)
-        {
-            for (int column=0; column<row; column++)
-            {
-                triangleContainer += "*";
-            }
-            triangleContainer += "\n";
-        }
-        return triangleContainer;
+        return getTriangle(10);
     }
 }
