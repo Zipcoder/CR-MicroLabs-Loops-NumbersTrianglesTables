92a351018bdb0edf6d60094183f95131a6b2acc8
Karousha Fennimore <karoushafennimore@zipcoders-MacBook-Pro-78.local>
2018-02-08 16:38:57 -0500

    Complete

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,28 +1,69 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import java.util.Scanner;
+
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evenNumbers = "";
+
+        for(int i= start; i <= stop; i++) {
+            if(i % 2 == 0) {
+                evenNumbers += Integer.toString(i);
+            }
+        }
+        return evenNumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddNumbers = "";
+
+        for(int i= start; i <= stop; i++) {
+            if(i % 2 != 0) {
+                oddNumbers += Integer.toString(i);
+            }
+        }
+        return oddNumbers;
     }
 
 
+
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squareNums = "";
+
+            for (int i = start; i < stop; i += step) {
+                int answer = (int)(Math.pow(i, 2));
+                squareNums += Integer.toString(answer);
+                }
+
+        return squareNums;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        StringBuilder newRange = new StringBuilder();
+
+            for (int i = start; i < stop; i += step) {
+                newRange.append(i);
+            }
+            String completeRange = newRange.toString();
+
+            return completeRange;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+
+        StringBuilder newValues = new StringBuilder();
+
+        for (int i = start; i < stop; i += step) {
+            int answer = (int) (Math.pow(i, exponent));
+            newValues.append(answer);
+        }
+        String completeValues = newValues.toString();
+
+        return completeValues;
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,54 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
+
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        StringBuilder smallMult = new StringBuilder();
+
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+
+            smallMult.append(String.format("%3d |", i*j));
+            }
+        smallMult.append("\n");
+        }
+        String completedTable = smallMult.toString();
+        return completedTable;
     }
 
+
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        StringBuilder largeMult = new StringBuilder();
+
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+
+                largeMult.append(String.format("%3d |", i*j));
+            }
+            largeMult.append("\n");
+        }
+        String completedTable = largeMult.toString();
+        return completedTable;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder multTable = new StringBuilder();
+
+        for(int i = 1; i <= tableSize; i++) {
+            for(int j = 1; j <= tableSize; j++) {
+                multTable.append(String.format("%3d |", i*j));
+            }
+            multTable.append("\n");
+        }
+
+        String completedTable = multTable.toString();
+        return completedTable;
+
     }
+
 }
