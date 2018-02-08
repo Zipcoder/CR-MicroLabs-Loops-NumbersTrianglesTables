15da80dd5ddb14971ee4de731f3fe1f10971f7a2
Corinne Winarski <corinnewinarski@zipcoders-MacBook-Pro-2.local>
2018-02-08 12:00:20 -0500

    First Refactor

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,38 +3,12 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
 
-        StringBuilder getSmol = new StringBuilder();
-
-        for (int r = 1; r <= 5; r++){
-
-            for(int c = 1; c <= 5; c++){
-
-                getSmol.append(String.format("%3d |", r*c));
-
-            }
-
-            getSmol.append("\n");
-        }
-
-        return getSmol.toString();
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
 
-        StringBuilder getBig = new StringBuilder();
-
-        for (int r = 1; r <= 10; r++){
-
-            for(int c = 1; c <= 10; c++){
-
-                getBig.append(String.format("%3d |", r*c));
-
-            }
-
-            getBig.append("\n");
-        }
-
-        return getBig.toString();
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -4,62 +4,34 @@ public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
 
-        String getTriangle = "";
+        String result = "";
 
-        for (int row = 1; row < numberOfRows; row++){
-
-            for (int column = 0; column < row; column++){
-
-                getTriangle += "*";
-            }
-            getTriangle += "\n";
+        for (int iteration = 1; iteration < numberOfRows; iteration++) {
+            result += getRow(iteration) +  "\n";
         }
 
-        return getTriangle;
+        return result;
     }
 
     public static String getRow(int numberOfStars) {
 
         String getRow = "";
 
-        for (int row = 0; row < numberOfStars; row++){
+        for (int row = 0; row < numberOfStars; row++) {
 
             getRow += "*";
         }
 
         return getRow;
-        
+
     }
 
     public static String getSmallTriangle() {
-
-        String triangle = "";
-
-        for (int row = 1; row <= 4; row++){
-
-            for (int column = 0; column < row; column++){
-
-                triangle += "*";
-            }
-            triangle += "\n";
-        }
-
-        return triangle;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
 
-        String largeTriangle = "";
-
-        for (int row = 1; row <= 9; row++){
-
-            for (int column = 0; column < row; column++){
-
-                largeTriangle += "*";
-            }
-            largeTriangle += "\n";
-        }
-
-        return largeTriangle;
+        return getTriangle(10);
     }
 }
