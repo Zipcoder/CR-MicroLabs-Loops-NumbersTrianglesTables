2358a3ed8d2f9da7737bd94a96be187b3ab96d86
Corinne Winarski <corinnewinarski@zipcoders-MacBook-Pro-2.local>
2018-02-07 19:39:00 -0500

    Finished First Part

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,26 +3,88 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder even = new StringBuilder();
+
+
+        if (start %2 != 0){
+
+            start += 1;
+
+        }
+
+        for (int i = start; i < stop; i += 2){
+
+            even.append(i);
+        }
+
+        return even.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder odd = new StringBuilder();
+
+        if (start %2 == 0){
+
+            start -= 1;
+
+        }
+
+        for (int i = start; i < stop; i += 2){
+
+            odd.append(i);
+        }
+
+        return odd.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        StringBuilder square  = new StringBuilder();
+
+        for (int i = start; i < stop; i += step)  {
+
+            int sqrRoot = (int) Math.pow(i,2);
+            square.append(sqrRoot);
+        }
+
+        return square.toString();
+
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        StringBuilder range = new StringBuilder();
+
+        for (int i = start; i < stop; i += step){
+
+             range.append(i);
+        }
+
+        return range.toString();
+
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+
+        StringBuilder expon = new StringBuilder();
+
+        for (int i = start; i < stop; i += step){
+
+            int exponVar = exponent;
+
+            int startExpon = (int) Math.pow(i, exponVar);
+
+            expon.append(startExpon);
+
+        }
+
+        return expon.toString();
+
     }
 }
