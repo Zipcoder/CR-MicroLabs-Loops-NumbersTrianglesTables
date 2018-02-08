c1bd66b478e46e5192b7be99bfb4313ceb243558
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-08 18:31:03 -0500

    msg

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,21 +3,60 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        String evenvalue = "";
+
+        for (int i = start; i<stop; i++) {
+
+            if (i %2 == 0){
+
+                evenvalue += "" + i;
+            }
+        }
+        return evenvalue;
+
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        String oddvalue = "";
+
+        for (int i = start; i < stop; i++) {
+
+            if (i%2 != 0 ) {
+
+                oddvalue += "" + i;
+
+            }
+        }
+        return oddvalue;
+
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        String storesqnumbers = "";
+    //int i is equal and while i is less then stop, i increases by steps increments(may have worded wrong)
+        for (int i = start; i < stop; i += step) {
+    //I am now concatenating into my storesqnumbers and I had to cast, on Math.pow because I was getting a float
+                storesqnumbers += + (int)Math.pow(i,2);
+        }
+        //returning storesqnumbers
+        return storesqnumbers;
     }
 
 
     public static String getRange(int start, int stop, int step) {
+
+
+
+
+
+
+
+
         return null;
     }
 
