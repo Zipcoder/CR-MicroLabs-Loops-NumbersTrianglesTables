23da1dfc77d85fcde10211f97d65c5007f6c40f6
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-02-08 11:32:12 -0500

    NumbersTrianglesTables Lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,37 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+       StringBuilder sb = new StringBuilder();
+       for (int i = 1; i <=5; i++){
+           for (int j = 1; j <=5; j++){
+               sb.append(String.format("%3d |", i *j));
+           }
+           sb.append("\n");
+       }
+       return sb.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <=10; i++){
+            for (int j = 1; j <=10; j++){
+                sb.append(String.format("%3d |", i *j));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
+
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <=tableSize; i++){
+            for (int j = 1; j <=tableSize; j++){
+                sb.append(String.format("%3d |", i *j));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
-}
+    }
+
