dfa35b1f7cd3a3c95c26a83e828f9610a9d131b4
Corinne Winarski <corinnewinarski@zipcoders-MacBook-Pro-2.local>
2018-02-07 20:59:28 -0500

    Finished Second Part

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,56 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        StringBuilder getSmol = new StringBuilder();
+
+        for (int r = 1; r <= 5; r++){
+
+            for(int c = 1; c <= 5; c++){
+
+                getSmol.append(String.format("%3d |", r*c));
+
+            }
+
+            getSmol.append("\n");
+        }
+
+        return getSmol.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        StringBuilder getBig = new StringBuilder();
+
+        for (int r = 1; r <= 10; r++){
+
+            for(int c = 1; c <= 10; c++){
+
+                getBig.append(String.format("%3d |", r*c));
+
+            }
+
+            getBig.append("\n");
+        }
+
+        return getBig.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder getSmol = new StringBuilder();
+
+        for (int r = 1; r <= tableSize; r++){
+
+            for(int c = 1; c <= tableSize; c++){
+
+                getSmol.append(String.format("%3d |", r*c));
+
+            }
+
+            getSmol.append("\n");
+        }
+
+        return getSmol.toString();
     }
 }
