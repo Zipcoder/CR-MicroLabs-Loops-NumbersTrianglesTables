8dc503586c1d3e1355fd33f880582c32452909df
Ahson Chaudhary <ahsonchaudhary@zipcodersMBP82.fios-router.home>
2018-02-09 00:52:48 -0500

    Finally Complete

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,22 +3,68 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        String outcome = "";
+
+        for (int i = start; i <= stop; i++){
+
+            if (i % 2 == 0)
+            {
+
+            outcome += i;
+
+            }
+
+        }
+
+        return outcome;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        String outcome = "";
+
+        for (int i = start; i <= stop; i++)
+
+        {
+            if (i % 2 != 0)
+            {
+
+                outcome += i;
+
+            }
+        }
+
+
+        return outcome;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        String outcome = "";
+
+        for (int i = start; i < stop; i += step)
+        {
+             outcome += i * i;
+        }
+
+        return outcome;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        String outcome = "";
+
+        for (int i = start; i < stop; i += step)
+        {
+            outcome += i;
+
+        }
+
+        return outcome;
     }
 
 
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,68 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+
+public class TableUtilities
+{
+    public static String getSmallMultiplicationTable()
+    {
+        //stringBuilder are mutable/can be changed
+        StringBuilder smallMultiplicationTable = new StringBuilder();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j<= 5; j++)
+            {
+
+                int num = i * j;
+                smallMultiplicationTable.append(String.format("%3d |", num));
+
+            } smallMultiplicationTable.append("\n");
+
+
+        }
+
+        String myTable = smallMultiplicationTable.toString();
+        return myTable;
+
+
+
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        StringBuilder largeMultiplicationTable = new StringBuilder();
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            for (int j = 1; j <= 10; j++)
+            {
+                int num = i  * j;
+                largeMultiplicationTable.append(String.format("%3d |", num));
+
+            } largeMultiplicationTable.append("\n");
+        }
+
+        String largeTable = largeMultiplicationTable.toString();
+        return largeTable;
     }
 
     public static String getMultiplicationTable(int tableSize) {
+
+        StringBuilder multiplicationTable = new StringBuilder();
+
+        for (int i = 0; i <= tableSize; i++){
+
+            for (int j = 0; j <= tableSize; j++)
+            {
+                int num = i * j;
+                multiplicationTable.append(String.format("%3d |", num));
+
+            }
+
+        }
+
+
         return null;
     }
 }
