7a4e8c0018df2af8eb15701e2fc00ec36d72056a
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-08 19:01:32 -0500

    ignore this it's just an inital iteration. i'll let you know when it's finished tsm

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,7 +2,24 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String myTable = "";
+
+        for (int row =1; row <=5; row++){
+            for (int col=1; col<=5; col++){
+
+                int checkSpace = col*row;
+                if (checkSpace < 10) {
+                    myTable += " " + checkSpace + " | ";
+                } else if (checkSpace >= 10){
+                    myTable += " " + checkSpace + " | ";
+                }
+
+
+            }
+            myTable += "\n";
+        }
+
+        return myTable;
     }
 
     public static String getLargeMultiplicationTable() {
