6bf86cc54dd8d36710cba02f6473d2834ebb7558
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-08 20:58:17 -0500

    FINALLLLLLLLLLLLL

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -14,7 +14,6 @@ public class TableUtilities {
                     myTable += " " + checkSpace + " | ";
                 }
 
-
             }
             myTable += "\n";
         }
@@ -23,10 +22,27 @@ public class TableUtilities {
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder getLarge = new StringBuilder();
+        for (int row=1; row <11 ; row++){
+            for (int col=1; col<11; col++){
+                getLarge.append(col*row + " | ");
+            }
+           getLarge.append("\n");
+
+        }
+
+        return getLarge.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder getMult = new StringBuilder();
+        for (int row = 1; row <= tableSize; row++){
+            for (int col = 1; col <= tableSize; col++){
+                getMult.append(row * col + " | ");
+            }
+            getMult.append("\n");
+        }
+
+        return getMult.toString();
     }
 }
