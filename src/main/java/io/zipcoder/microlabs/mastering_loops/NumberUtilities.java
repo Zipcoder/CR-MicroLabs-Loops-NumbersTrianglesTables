8877e8f85ecc2a1b36b44fe4f3846503ef5e2e53
Sean Mis <seanmis@MacBook-Pro-3.local>
2018-02-08 19:47:20 -0500

    NTT Totally Done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,26 +3,50 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evens = "";
+        for(int i = start; i < stop; i++) {
+            if(i % 2 == 0) {
+                evens += Integer.toString(i);
+            }
+        }
+        return evens;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String odds = "";
+        for(int i = start; i < stop; i++) {
+            if(i % 2 != 0) {
+                odds += Integer.toString(i);
+            }
+        }
+        return odds;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squares = "";
+        for(int i = start; i < stop; i+=step) {
+                squares += Integer.toString(i*i);
+        }
+        return squares;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for(int i = start; i < stop; i+=step) {
+            range += Integer.toString(i);
+        }
+        return range;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String expo = "";
+        for(int i = start; i < stop; i+=step) {
+            expo += (int) Math.pow(i, exponent);
+        }
+        return expo;
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,38 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder smallMult = new StringBuilder();
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                int colRow = col * row;
+                smallMult.append(String.format("%3d |", colRow));
+            }
+            smallMult.append("\n");
+        }
+        return smallMult.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder largeMult = new StringBuilder();
+        for (int row = 1; row <= 10; row++) {
+            for (int col = 1; col <= 10; col++) {
+                int colRow = col * row;
+                largeMult.append(String.format("%3d |", colRow));
+            }
+            largeMult.append("\n");
+        }
+        return largeMult.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder multTab = new StringBuilder();
+        for (int row = 1; row <= tableSize; row++) {
+            for (int col = 1; col <= tableSize; col++) {
+                int colRow = col * row;
+                multTab.append(String.format("%3d |", colRow));
+            }
+            multTab.append("\n");
+        }
+        return multTab.toString();
     }
 }
