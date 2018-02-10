b4797f4d5a307f6a35fd42daf907bb1642021165
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-3.local>
2018-02-10 17:18:25 -0500

    sunday

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,78 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+
+        StringBuilder outcome = new StringBuilder();
+
+
+        for (int i=1; i < numberOfRows; i++) {
+
+            for (int j=0; j < i; j++){
+
+                outcome.append("*");
+            }
+                outcome.append("\n");
+
+            }
+
+
+            return outcome.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+
+        StringBuilder outcome = new StringBuilder();
+
+
+        for (int i=0; i < numberOfStars; i++){
+
+            outcome.append("*");
+
+        }
+
+
+        return outcome.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        StringBuilder outcome = new StringBuilder();
+
+
+        for (int i=1; i <=4; i++) {
+
+            for (int j=0; j < i; j++){
+
+                outcome.append("*");
+            }
+            outcome.append("\n");
+
+        }
+
+
+        return outcome.toString();
+
+
+
     }
 
     public static String getLargeTriangle() {
-        return null;
+
+        StringBuilder outcome = new StringBuilder();
+
+
+        for (int i=1; i <=9; i++) {
+
+            for (int j=0; j < i; j++){
+
+                outcome.append("*");
+            }
+            outcome.append("\n");
+
+        }
+
+
+        return outcome.toString();
+
+
     }
 }
