de106b7ce6b4235e3b7cc8593ff8db03f85ecc2c
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-3.local>
2018-02-12 10:40:18 -0500

    plug'n

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -5,11 +5,36 @@ public class TableUtilities {
         return null;
     }
 
+
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        StringBuilder theString = new StringBuilder();
+
+        for (int i = 1; i<11; i++) {
+
+            for (int j = 1; j<11; j++) {
+
+                theString.append(String.format("%3d |", i * j));
+            }
+            theString.append("\n");
+        }
+        return theString.toString();
     }
 
+
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder theString = new StringBuilder();
+
+        for (int i = 1; i <= tableSize; i++) {
+
+            for (int j = 1; j <= tableSize; j++) {
+
+                theString.append(String.format("%3d |", i * j));
+            }
+            theString.append("\n");
+        }
+        return theString.toString();
+
     }
 }
