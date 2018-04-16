f4e92ec94be0f8d70d117a2b8c2267af1c815cec
Kthomas <kthomas@github.com>
2018-04-16 11:36:49 -0400

    Refactor 2

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,45 +3,47 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TableUtilities {
 
     public String getSmallMultiplicationTable() {
-
-        String lilguy = "";
-            for(int i = 1; i < 6; i++) {
-                String grow = "";
-                for (int a = 1; a < 6; a++) {
-                    int b = a * i;
-                        grow += String.format("%3d", b) + " |";
-                    }
-                lilguy += grow + "\n";
+        StringBuilder smallTable = new StringBuilder();
+        for (int i = 1; i < 6; i++) {
+            StringBuilder grow = new StringBuilder();
+            for (int a = 1; a < 6; a++) {
+                int b = a * i;
+                grow.append(String.format("%3d", b)).append(" |");
             }
-        return lilguy;
+
+            smallTable.append(grow).append("\n");
+        }
+
+        return smallTable.toString();
     }
 
     public String getLargeMultiplicationTable() {
-
-        String bigguy = "";
-        for(int i = 1; i < 11; i++) {
-            String grow = "";
+        StringBuilder bigTable = new StringBuilder();
+        for (int i = 1; i < 11; i++) {
+            StringBuilder grow = new StringBuilder();
             for (int a = 1; a < 11; a++) {
                 int b = a * i;
-                grow += String.format("%3d", b) + " |";
+                grow.append(String.format("%3d", b)).append(" |");
             }
-            bigguy += grow + "\n";
+
+            bigTable.append(grow).append("\n");
         }
-        return bigguy;
+
+        return bigTable.toString();
     }
 
     public String getMultiplicationTable(int tableSize) {
-
-        String ultraguy = "";
+        StringBuilder table = new StringBuilder();
         for (int i = 1; i < 21; i++) {
-            String grow = "";
+            StringBuilder grow = new StringBuilder();
             for (int a = 1; a < 21; a++) {
                 int b = a * i;
-                grow += String.format("%3d", b) + " |";
+                grow.append(String.format("%3d", b)).append(" |");
             }
-            ultraguy += grow + "\n";
+
+            table.append(grow).append("\n");
         }
 
-            return ultraguy;
-        }
+        return table.toString();
     }
+}
