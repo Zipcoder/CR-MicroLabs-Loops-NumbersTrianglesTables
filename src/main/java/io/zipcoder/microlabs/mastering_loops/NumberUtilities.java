d3ee7806da9fe9768c05941c693436666edac8ca
Kthomas <kthomas@github.com>
2018-04-16 11:30:45 -0400

    Refactor

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,93 +1,66 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = start; i < stop; i++) {
-
             if (i % 2 == 0) {
-
-                numList = numList + i;
+                numList.append(i);
             }
         }
 
-
-        return numList;
+        return numList.toString();
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = start; i < stop; i++) {
-
             if (i % 2 != 0) {
-
-                numList = numList + i;
+                numList.append(i);
             }
         }
-        return numList;
-    }
 
+        return numList.toString();
+    }
 
     public static String getSquareNumbers(int start, int stop, int step) {
-
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = start; i < stop; i += step) {
-
-            int theLid = (int) Math.pow(i, 2);
-            numList += String.valueOf(theLid);
-
+            int x = (int) Math.pow(i, 2);
+            numList.append(String.valueOf(x));
         }
 
-        return numList;
+        return numList.toString();
     }
-
 
     public static String getRange(int stop) {
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = 0; i < stop; i++)
-            numList += String.valueOf(i);
-
-        return numList;
+            numList.append(String.valueOf(i));
+        return numList.toString();
     }
-
-
 
     public static String getRange(int start, int stop) {
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = start; i < stop; i++)
-            numList += String.valueOf(i);
-
-        return numList;
+            numList.append(String.valueOf(i));
+        return numList.toString();
     }
-
 
     public static String getRange(int start, int stop, int step) {
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = start; i < stop; i+=step)
-            numList += String.valueOf(i);
-
-        return numList;
+            numList.append(String.valueOf(i));
+        return numList.toString();
     }
 
-
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-
-        String numList = "";
-
+        StringBuilder numList = new StringBuilder();
         for (int i = start; i < stop; i += step) {
-
-            int theLid = (int) Math.pow(i, exponent);
-            numList += String.valueOf(theLid);
+            int x = (int) Math.pow(i, exponent);
+            numList.append(String.valueOf(x));
         }
-        return numList;
+
+        return numList.toString();
     }
 }
